ff9b8354fd845812df8bf28b9ef265d5da57f41a
Developing-Developer <60450322+Developing-Developer@users.noreply.github.com>
2021-03-26 14:39:52 -0500

    Main Menu + Arrow Ricochet physics
    Added basic main menu and arrow ricochet physics

--- Ricochet/Assets/Scripts/Bow.cs
+++ Ricochet/Assets/Scripts/Bow.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
     public Vector2 direction;
+    //float moveSpeed = 2f;
+    //private float moveDirection;
 
+    private void Awake()
+    {
+        //rb = transform.GetComponent<Rigidbody2D>();
+        //boxCollider = transform.GetComponent<BoxCollider2D>();
+    }
     void Update()
     {
         Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 bowPos = transform.position;
-
         direction = MousePos - bowPos;
-
         FaceMouse();
+
+        //moveDirection = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        //Move();
     }
 
     void FaceMouse()
     {
         transform.right = direction;
     }
+
+    /*void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }*/
 }
