f261893fea0266a281283cc01d9bfc2121ac8e7e
Ben Bordelon <bbord32@lsu.edu>
2021-03-26 10:15:03 -0500

    LEVEL1
    Basic game level with functioning bow and arrow and prefabs

---
+++ Ricochet/Assets/Scripts/Bow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    public Vector2 direction;
+
+    void Update()
+    {
+        Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 bowPos = transform.position;
+
+        direction = MousePos - bowPos;
+
+        FaceMouse();
+    }
+
+    void FaceMouse()
+    {
+        transform.right = direction;
+    }
+}
---
+++ Ricochet/Assets/Scripts/ShootArrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootArrow : MonoBehaviour
+{
+
+    public float LaunchForce;
+
+    public GameObject Arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ArrowShoot();
+        }
+        
+    }
+
+    void ArrowShoot()
+    {
+        GameObject ArrowIns = Instantiate(Arrow, transform.position, transform.rotation);
+        ArrowIns.GetComponent<Rigidbody2D>().AddForce(transform.right * LaunchForce);
+    }
+}
