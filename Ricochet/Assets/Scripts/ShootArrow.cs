5229a6833c936331b6658b6bd3b6c32651e859b4
Ben Bordelon <bbord32@lsu.edu>
2021-03-29 12:29:20 -0500

    LEVEL1FINAL
    implemented retry button and game over scene

--- Ricochet/Assets/Scripts/ShootArrow.cs
+++ Ricochet/Assets/Scripts/ShootArrow.cs
@@ -29,7 +29,7 @@ public class ShootArrow : MonoBehaviour
         {
             if (arrowCount == 0)
             {
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(3);
                 arrowCount++;
             }
 
