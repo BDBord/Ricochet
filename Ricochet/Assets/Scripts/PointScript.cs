9b95a1e6d3d2b4b32b4b1ea5d09bbb064bf1c225
agent3c <agent3c@yahoo.com>
2021-04-19 14:32:27 -0500

    Redid everything I did last night

---
+++ Ricochet/Assets/Scripts/PointScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointScript : MonoBehaviour
+{
+    [HideInInspector]
+    public bool hasHitCollider = false;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        hasHitCollider = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        hasHitCollider = false;
+    }
+}
