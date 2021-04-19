dadefe49cfced6414e32119a6040b6c889a9122a
Developing-Developer <60450322+Developing-Developer@users.noreply.github.com>
2021-04-19 18:30:02 -0500

    Fixed things
    Fixed button functionality.
    Changed #(enemies) for tut level to 2.
    Stretched the image in Victory to cover for blank spots.

--- Ricochet/Assets/Scripts/MainMenuScript.cs
+++ Ricochet/Assets/Scripts/MainMenuScript.cs
@@ -24,4 +24,39 @@ public class MainMenuScript : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(11);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void Level6()
+    {
+        SceneManager.LoadScene(6);
+    }
 }
